418fd25b9d09135701d3ab708ba50c06cc96143c
ohabbestad <ole.habbestad@gmail.com>
2026-03-12 20:21:40 +0100

    Lagt inn tailwind.css

---
+++ koding/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./del1/**/*.html",
+    "./del2/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
